61f2d743aae07e137941f6bce4f753beecf54d12
Servant-of-Scietia <samsun2006@icloud.com>
2024-03-28 18:34:36 +1300

    REEEEEELLLLUUU

--- deep_feedforward_networks/activation_functions.h
+++ deep_feedforward_networks/activation_functions.h
@@ -6,15 +6,27 @@
 
 namespace activation_functions
 {
-    int heaviside_step(int value)
     /**
      * Implements the heaviside_step function used in the very beginnings of deep learning.
      * Simulates the behaviour of a biological neuron.
      * @cite https://en.wikipedia.org/wiki/Heaviside_step_function
     */
+    double heaviside_step(double value)
     {
         return value>=0;
     }
+
+  
+    /**
+     * @attention ReLU function
+     * g(z)=max(0,z)
+    */
+    double ReLU(double value)
+    {
+        return std::max(value,0.0);
+    }
+
+
 }
 
 
