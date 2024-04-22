86c52ca7d44a957e8446ba649bab13ccac072f00
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-22 13:17:44 +1200

    bug fix

--- layer/layer.h
+++ layer/layer.h
@@ -12,7 +12,6 @@ protected:
     std::vector<double> __input; 
     
     LAYER(){};
-    ~LAYER();
     virtual std::vector<double> operation(std::vector<double>&) =0; // forward pass over layer
     virtual void differentiate() =0;   // calculate part of the chain rule
     virtual std::vector<double> backpropagation(std::vector<double>&) =0; // backward pass over the layer 
