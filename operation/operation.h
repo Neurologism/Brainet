54ecab0e4e2ae706463de55b75a95a64fb3b04cb
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-24 17:29:12 +1200

    update to template

--- operation/operation.h
+++ operation/operation.h
@@ -7,17 +7,14 @@
 /**
  * @brief OPERATION class is an abstract class that defines the interface for all operations.
 */
-
+template <typename T>
 class OPERATION
 {
-protected:
-    VARIABLE * __variable;
 public:
-    OPERATION(VARIABLE * variable) : __variable(variable){};
     /**
      * @brief mathematical function the operation implements
     */
-    virtual void f(std::vector<VARIABLE *>& inputs) =0; 
+    virtual void f(std::vector<T *>& inputs) =0; 
     /**
      * @brief derivative of the function
      * assumes that the gradient is already calculated for the output variables 
@@ -25,7 +22,7 @@ public:
      * @param inputs the input variables of the operation
      * @param outputs the output variables of the operation
     */
-    virtual void bprop(std::vector<VARIABLE *>& inputs, std::vector<VARIABLE *> outputs) =0;
+    virtual void bprop(std::vector<T *>& inputs, std::vector<T *> outputs) =0;
 };
 
 #endif // OPERATION_INCLUDE_GUARD
