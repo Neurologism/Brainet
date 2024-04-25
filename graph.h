5a76e2f1708c15d9405f46463ebcf62a87b0dabc
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-25 17:51:54 +1200

    add graph class

--- graph.h
+++ graph.h
@@ -4,13 +4,31 @@
 
 #include "variable.h"
 
-class GRAPH
+class GRAPH // dag of variables and operations
 {
-    
+    std::vector<VARIABLE> __variables;
+    std::vector<OPERATION> __operations;
 public:
     GRAPH();
     ~GRAPH();
+    VARIABLE * operator[](int index);
+
 }; 
+
+GRAPH::GRAPH()
+{
+}
+
+GRAPH::~GRAPH()
+{
+}   
+
+VARIABLE * GRAPH::operator[](int index)
+{
+    return &__variables[index];
+}
+
+
 
 
 #endif // GRAPH_INCLUDE_GUARD
