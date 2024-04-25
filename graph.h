9ba48486d8f46546605459a995c9fdc317f8637e
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-25 22:33:10 +1200

    add comments

--- graph.h
+++ graph.h
@@ -42,7 +42,7 @@ std::vector<std::vector<double>> GRAPH::backprop(std::vector<bool> & target, int
 
     for (int i = 0; i < __variables.size(); i++)
     {
-        if (target[i])
+        if (target[i]) // call build grad for each target variable
         {
             build_grad(i, grad_table);
         }
@@ -51,24 +51,29 @@ std::vector<std::vector<double>> GRAPH::backprop(std::vector<bool> & target, int
     
 }
 
-
+/**
+ * @brief builds the gradient table for the variable with id focus using dp with dfs 
+ * @param focus the variable to be differentiated
+ * @param grad_table the gradient table to be built
+*/
 void GRAPH::build_grad(int focus, std::vector<std::vector<double>> & grad_table)
 {
-    if (grad_table[focus].size() != 0)
+    if (grad_table[focus].size() != 0) // gradient of this variable already computed
     {
         return;
     }
 
-    grad_table[focus].resize(__variables[focus].get_data().size());
+    grad_table[focus].resize(__variables[focus].get_data().size()); // make space for the gradient
 
-    for (int i = 0; i < __variables[focus].get_consumers().size(); i++)
+    for (int i = 0; i < __variables[focus].get_consumers().size(); i++) // the sum of the gradients of the consumers is the gradient of the variable
     {
+        // load stuff
         VARIABLE * consumer = __variables[focus].get_consumers()[i];
         OPERATION * op = consumer->get_operation();
         std::vector<VARIABLE *> inputs = consumer->get_inputs();
-        build_grad(consumer->get_id(), grad_table);
-        std::vector<double> gradient = op->bprop(consumer->get_inputs(), __variables[focus], grad_table[consumer->get_id()]);
-        for (int j = 0; j < gradient.size(); j++)
+        build_grad(consumer->get_id(), grad_table); // build the gradient table for the consumer (dp, dfs)
+        std::vector<double> gradient = op->bprop(consumer->get_inputs(), __variables[focus], grad_table[consumer->get_id()]); // calculate the gradient of the consumer with respect to the focus variable
+        for (int j = 0; j < gradient.size(); j++) // add the gradient to the gradient table
         {
             grad_table[focus][j] += gradient[j];
         }
