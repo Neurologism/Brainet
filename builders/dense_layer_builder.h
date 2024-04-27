869136301ad2060a15edb467c70813b0872b339b
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-27 21:59:35 +1200

    add dense_layer_builder

---
+++ builders/dense_layer_builder.h
@@ -0,0 +1,16 @@
+#ifndef DENSE_LAYER_BUILDER_INCLUDE_GUARD
+#define DENSE_LAYER_BUILDER_INCLUDE_GUARD
+
+#include "dependencies.h"
+
+/**
+ * @brief DENSE_LAYER_BUILDER class is a builder class for creating dense layers in a graph of operations.
+*/
+
+class DENSE_LAYER_BUILDER
+{
+public:
+    
+}
+
+#endif // DENSE_LAYER_BUILDER_INCLUDE_GUARD
