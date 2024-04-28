e12a3e41199b3de2256fd2a51247a42dc2d5616e
Servant-of-Scietia <samsun2006@icloud.com>
2024-04-28 17:55:29 +1200

    add builder as friend of graph

---
+++ builders/builder.h
@@ -0,0 +1,17 @@
+#ifndef BUILDER_INCLUDE_GUARD
+#define BUILDER_INCLUDE_GUARD
+
+#include "graph.h"
+#include "variable.h"
+#include "dependencies.h"
+
+/**
+ * @brief BUILDER class is a builder class for turning a model in a graph out of operations.
+*/
+
+class BUILDER
+{
+    
+    static VARIABLE * __end_of_stream;
+};
+#endif // BUILDER_INCLUDE_GUARD
