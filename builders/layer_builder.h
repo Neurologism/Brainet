1712eb57888c19367a6f9cd795c6d4497eb274ce
Servant-of-Scietia <samsun2006@icloud.com>
2024-05-07 14:51:22 +1200

    add matrix multiplication

--- builders/layer_builder.h
+++ builders/layer_builder.h
@@ -2,7 +2,9 @@
 #define LAYER_BUILDER_INCLUDE_GUARD
 
 #include "builder.h"
+#include "../dependencies.h"
 #include "../operation/activation_function/rectified_linear_unit.h"
+#include "../operation/linear_algebra/matmul.h"
 
 /**
  * @brief LAYER_BUILDER class is a builder class for creating a layer in a model.
@@ -17,12 +19,18 @@ public:
     void add_activation_function();
 };
 
+void LAYER_BUILDER::add_matrix_multiplication()
+{
+    if(__end_of_stream!=nullptr) throw std::exception("End of stream is not set.");
+    VARIABLE * variable = new VARIABLE(new MATMUL(), {__end_of_stream}, {});
+}
+
 
 
 void LAYER_BUILDER::add_activation_function(OPERATION * activation_function)
 {
-    if(__end_of_stream!=nullptr) VARIABLE * variable = new VARIABLE(activation_function, {__end_of_stream}, {});
-    else VARIABLE * variable = new VARIABLE(activation_function, {}, {});
+    if(__end_of_stream!=nullptr) throw std::exception("End of stream is not set.");
+    VARIABLE * variable = new VARIABLE(activation_function, {__end_of_stream}, {});
     __end_of_stream->get_consumers().push_back(variable);
     __graph->add_variable(variable);
     __end_of_stream = variable;
