674931e39d1d9c89d13b7f6fa8c975a3ff4b06be
Servant-of-Scietia <samsun2006@icloud.com>
2024-06-20 16:11:03 +1200

    add constructor when only the operation is known

--- variable.h
+++ variable.h
@@ -19,14 +19,15 @@ class VARIABLE
     int __id;
 
 public:
+    VARIABLE(OPERATION * op) : __op(op){__id = __counter++;};
     VARIABLE(OPERATION * op, std::vector<VARIABLE *> parents, std::vector<VARIABLE *> children) : __op(op), __parents(parents), __children(children){__id = __counter++;};
     ~VARIABLE();
     OPERATION * get_operation();
     std::vector<VARIABLE *> & get_consumers();
     std::vector<VARIABLE *> & get_inputs();
     TENSOR<double> * get_data();
-    int get_id();
     void set_data(TENSOR<double> & data);
+    int get_id();
 };
 
 VARIABLE::~VARIABLE()
