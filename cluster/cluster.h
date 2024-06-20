502ac192a780edbfed61cc175cdca228b0e67063
Servant-of-Scietia <samsun2006@icloud.com>
2024-06-20 16:57:28 +1200

    constructor of cluster sets the graph

--- cluster/cluster.h
+++ cluster/cluster.h
@@ -3,6 +3,7 @@
 
 #include "../dependencies.h"
 #include "../variable.h"
+#include "../graph.h"
 
 /**
  * @brief CLUSTER class is a wrapper class for a group of variables and operations.
@@ -11,7 +12,10 @@ class CLUSTER
 {
     std::vector<VARIABLE *> __variables, __inputs, __outputs;
     std::vector<OPERATION *> __operations;
+    static GRAPH * __graph = nullptr;
 public:
+    // constructor should be called to set graph
+    CLUSTER(GRAPH * graph) { __graph = graph; }
     std::vector<VARIABLE *> & get_variables() { return __variables; }
     std::vector<VARIABLE *> & get_inputs() { return __inputs; }
     std::vector<VARIABLE *> & get_outputs() { return __outputs; }
