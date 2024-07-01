0def6a554a9b7c3f1e54a54f35d615d8fe7cdb23
Servant-of-Scietia <samsun2006@icloud.com>
2024-07-01 12:58:01 +1200

    keep track of outputs

--- model.h
+++ model.h
@@ -9,10 +9,11 @@
 class MODEL
 {
     GRAPH __graph;
+    std::vector<VARIABLE *> __to_be_differentiated;
 public:
     MODEL(){CLUSTER::set_graph(&__graph);};
     void load();
-    void sequential(std::vector<CLUSTER_VARIANT> layers);
+    void sequential(std::vector<CLUSTER_VARIANT> layers, bool add_backprop = true);
     void train(int epochs, double learning_rate);
 };
 
@@ -21,7 +22,7 @@ void MODEL::load()
     CLUSTER::set_graph(&__graph);
 }
 
-void MODEL::sequential(std::vector<CLUSTER_VARIANT> layers)
+void MODEL::sequential(std::vector<CLUSTER_VARIANT> layers, bool add_backprop)
 {
     std::vector<CLUSTER*> clusters;
     for (CLUSTER_VARIANT& layer : layers) {
@@ -36,13 +37,15 @@ void MODEL::sequential(std::vector<CLUSTER_VARIANT> layers)
         clusters[i]->add_output(clusters[i+1]->input());
         clusters[i+1]->add_input(clusters[i]->output(),clusters[i]->size());
     }
+
+    if(add_backprop)__to_be_differentiated.push_back(clusters.back()->output());
 }
 
 void MODEL::train(int epochs, double learning_rate)
 {
     __graph.forward();
     std::vector<bool> v(__graph.get_variables().size(),true);
-    __graph.backprop(v,10);
+    std::vector<TENSOR<double>> grad_table = __graph.backprop(v, __to_be_differentiated);
 }
 
 #endif // MODEL_INCLUDE_GUARD
