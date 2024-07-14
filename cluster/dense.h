6628f27b0d15aec042f4fa533ed66da55ca32f35
Samuel Leber <samuel.leber@outlook.com>
2024-07-14 17:45:56 +0200

    activate matrix multiplication

--- cluster/dense.h
+++ cluster/dense.h
@@ -20,7 +20,7 @@ public:
     ~DENSE() = default;
     void add_input(std::shared_ptr<VARIABLE> input, int units) override
     {
-        _activation_variable->get_inputs().push_back(input);
+        _matmul_variable->get_inputs().push_back(input);
     }
     void add_output(std::shared_ptr<VARIABLE> output) override
     {
@@ -28,7 +28,7 @@ public:
     }
     std::shared_ptr<VARIABLE> input(int index) override
     {
-        return _activation_variable;
+        return _matmul_variable;
     }
     std::shared_ptr<VARIABLE> output(int index) override
     {
@@ -44,22 +44,22 @@ DENSE::DENSE(ACTIVATION_FUNCTION_VARIANT activation_function, int units)
     }
     __units = units;
 
-    // // create the variables
+    // create the variables
     
-    // _weight_matrix_variable = __graph->add_variable(VARIABLE(nullptr, {}, {})); // nullptr because there is no operation
+    _weight_matrix_variable = __graph->add_variable(std::make_shared<VARIABLE>(VARIABLE(nullptr, {}, {}))); // nullptr because there is no operation
 
-    // _matmul_variable = __graph->add_variable(VARIABLE(new MATMUL(), {_weight_matrix_variable}, {}));
+    _matmul_variable = __graph->add_variable(std::make_shared<VARIABLE>(VARIABLE(std::make_shared<MATMUL>(MATMUL()), {_weight_matrix_variable}, {})));
 
     // Use std::visit to handle the variant
     std::shared_ptr<OPERATION> operation_ptr = std::visit([](auto&& arg) {
         // Assuming all types in the variant can be dynamically casted to OPERATION*
         return std::shared_ptr<OPERATION>(std::make_shared<std::decay_t<decltype(arg)>>(arg));}, ACTIVATION_FUNCTION_VARIANT{activation_function});
 
-    _activation_variable = __graph->add_variable(std::make_shared<VARIABLE>(VARIABLE(operation_ptr, {}, {})));
+    _activation_variable = __graph->add_variable(std::make_shared<VARIABLE>(VARIABLE(operation_ptr, {_matmul_variable}, {})));
 
     // conections within the cluster
-    // _weight_matrix_variable->get_consumers()->push_back(_matmul_variable);
-    // _matmul_variable->get_consumers()->push_back(_activation_variable);    
+    _weight_matrix_variable->get_consumers().push_back(_matmul_variable);
+    _matmul_variable->get_consumers().push_back(_activation_variable);    
 }
 
 #endif // DENSE_INCLUDE_GUARD
