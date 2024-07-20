18d9b2944f903f8b1c2668c19f11abd1dab9d00f
Samuel Leber <samuel.leber@outlook.com>
2024-07-20 14:33:47 +0200

    add backprop

--- operation/cost_function/mse.h
+++ operation/cost_function/mse.h
@@ -32,8 +32,23 @@ void MSE::f(std::vector<std::shared_ptr<VARIABLE>>& inputs)
 }
 
 
+std::shared_ptr<TENSOR<double>> MSE::bprop(std::vector<std::shared_ptr<VARIABLE>>& inputs, std::shared_ptr<VARIABLE> & focus, std::shared_ptr<TENSOR<double>> & gradient)
+{
+    if(inputs.size() != 2)
+    {
+        throw std::runtime_error("MSE operation requires 2 inputs");
+    }
+    if(inputs[0]->get_data()->shape() != inputs[1]->get_data()->shape())
+    {
+        throw std::runtime_error("MSE operation requires inputs to have the same shape");
+    }
+    std::shared_ptr<TENSOR<double>> _gradient = std::make_shared<TENSOR<double>>(TENSOR<double>(inputs[0]->get_data()->shape()));
+    for(int i = 0; i < inputs[0]->get_data()->size(); i++)
+    {
+        _gradient->data()[i] = (inputs[0]->get_data()->at({i}) - inputs[1]->get_data()->at({i})) / inputs[0]->get_data()->size() * gradient->data()[i];
+    }
 
-
-
+    return _gradient;
+}
 
 #endif // MSE_INCLUDE_GUARD
