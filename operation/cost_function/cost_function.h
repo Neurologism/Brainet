8cf9320d57c44d144b2fbb2e62a435ec9f73b389
Samuel Leber <samuel.leber@outlook.com>
2024-07-20 14:51:32 +0200

    cost function variant

---
+++ operation/cost_function/cost_function.h
@@ -0,0 +1,10 @@
+#ifndef COST_FUNCTION_INCLUDE_GUARD
+#define COST_FUNCTION_INCLUDE_GUARD
+
+#include "../dependencies.h"
+#include "mse.h"
+
+
+using COST_FUNCTION_VARIANT = std::variant<MSE>;
+
+#endif // COST_FUNCTION_INCLUDE_GUARD
