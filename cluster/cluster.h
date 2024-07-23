3797a7306185b1643f826190bc2331f8b239b764
Servant-of-Scietia <samsun2006@icloud.com>
2024-07-23 18:27:25 +0200

    cleanup and documentation

--- cluster/cluster.h
+++ cluster/cluster.h
@@ -6,27 +6,44 @@
 #include "../graph.h"
 
 /**
- * @brief CLUSTER class is a wrapper class for a group of variables and operations.
+ * @brief The CLUSTER class can be used to group multiple variables together. This is useful for adding substructures to the graph. It is mainly used to add something similar to layers to the graph.
  */
 class CLUSTER
 {
 protected:
-    static std::shared_ptr<GRAPH> __graph;
-    static std::vector<std::shared_ptr<VARIABLE>> __learnable_parameters;
-    int __units = -1;
+    static std::shared_ptr<GRAPH> __graph; // shared by all clusters , points to the graph into which the cluster should add its variables
+    static std::vector<std::shared_ptr<VARIABLE>> __learnable_parameters; // shared by all clusters , points to the learnable parameters of the graph for later use in the optimization process
+    int __units = -1; // stores the input size of the cluster (could be moved to respective derived classes)
 public:
     virtual ~CLUSTER() = default;
-    virtual void add_input(std::shared_ptr<VARIABLE> input, int units){}
-    virtual void add_output(std::shared_ptr<VARIABLE> output){}
+    /**
+     * @brief virtual function that is supposed to be implemented by the derived classes. It is used to mark variables as input for the cluster. 
+     */
+    virtual void add_input(std::shared_ptr<VARIABLE> input, int units){};
+    /**
+     * @brief virtual function that is supposed to be implemented by the derived classes. It is used to mark variables as output for the cluster.
+     */
+    virtual void add_output(std::shared_ptr<VARIABLE> output){};
+    /**
+     * @brief virtual function that is supposed to be implemented by the derived classes. It is used to get the input variables of the cluster specified by the index.
+     */
     virtual std::shared_ptr<VARIABLE> input(int index = 0){return nullptr;}
+    /**
+     * @brief virtual function that is supposed to be implemented by the derived classes. It is used to get the output variables of the cluster specified by the index.
+     */
     virtual std::shared_ptr<VARIABLE> output(int index = 0){return nullptr;}
+    /**
+     * @brief wrapper class used to set the graph for all cluster objects manually. Manly intended for use with multiple graphs.
+     */
     static void set_graph(std::shared_ptr<GRAPH> graph){__graph = graph;}
-    int size()
+    /**
+     * @brief used to get the private variable __units, could be moved to the respective derived classes.
+     */
+    int getUnits()
     {
         if(__units == -1)throw std::runtime_error("units not set");
         return __units;
     }
-    int getUnits(){return __units;}
     static std::vector<std::shared_ptr<VARIABLE>> & get_learnable_parameters(){return __learnable_parameters;}
 };
 
