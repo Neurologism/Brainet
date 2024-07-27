e8edf40e6d351f71facece03895e1a6e7dd89c4d
Servant-of-Scietia <samsun2006@icloud.com>
2024-07-27 15:29:33 +0200

    Refactor MODEL class for better readability and maintainability

--- model.h
+++ model.h
@@ -6,15 +6,38 @@
 #include "cluster/cluster.h"
 #include "operation/activation_function/activation_function.h"
 
+/**
+ * @brief The model class is the main interface for the user to create a neural network. It is used to build the network and to train it.
+ * The model class combines the graph, the cluster and the operation classes to create a neural network.
+ */
 class MODEL
 {
-    std::shared_ptr<GRAPH> __graph = std::make_shared<GRAPH>();
-    std::vector<std::shared_ptr<VARIABLE>> __to_be_differentiated;
+    std::shared_ptr<GRAPH> __graph = std::make_shared<GRAPH>(); // the computational graph
+    std::vector<std::shared_ptr<VARIABLE>> __to_be_differentiated; // the variables that are to be differentiated
 public:
-    MODEL(){CLUSTER::set_graph(__graph);};
+    /**
+     * @brief Construct a new MODEL object.
+     */
+    MODEL()
+    {
+        CLUSTER::set_graph(__graph); // set the static graph pointer in the cluster class
+    };
     ~MODEL(){};
+    /**
+     * @brief This function loads the graph of the model into the cluster class.
+     */
     void load();
+    /**
+     * @brief This function creates a sequential neural network.
+     * @param layers The layers of the neural network.
+     * @param add_backprop If true the output of the last layer is added to the differentiation vector.
+     */
     void sequential(std::vector<CLUSTER_VARIANT> layers, bool add_backprop = true);
+    /**
+     * @brief This function trains the model. It uses the backpropagation algorithm to update the learnable parameters. 
+     * @param epochs The number of epochs.
+     * @param learning_rate The learning rate.
+     */
     void train(int epochs, double learning_rate);
 };
 
@@ -44,15 +67,16 @@ void MODEL::sequential(std::vector<CLUSTER_VARIANT> layers, bool add_backprop)
 
 void MODEL::train(int epochs, double learning_rate)
 {
+    // this should be replaced by a more sophisticated training algorithm
     for(int epoch = 0; epoch < epochs; epoch++)
     {
         __graph->forward();
         std::shared_ptr<TENSOR<double>> loss = __to_be_differentiated[0]->get_data();
-        std::cout << "Epoch: " << epoch << " Loss: " << loss->data()[0] << std::endl;
-        std::vector<std::shared_ptr<TENSOR<double>>> v = __graph->backprop(CLUSTER::get_learnable_parameters(), __to_be_differentiated);
+        std::cout << "Epoch: " << epoch << " Loss: " << loss->data()[0] << std::endl; // print loss
+        std::vector<std::shared_ptr<TENSOR<double>>> v = __graph->backprop(CLUSTER::get_learnable_parameters(), __to_be_differentiated); // backpropagation
         for(int i = 0; i < CLUSTER::get_learnable_parameters().size(); i++)
         {
-            std::cout << "Parameter " << i << " ";
+            std::cout << "Parameter " << i << " "; // debug
             for(int j = 0; j < CLUSTER::get_learnable_parameters()[i]->get_data()->size(); j++)
             {
                 CLUSTER::get_learnable_parameters()[i]->get_data()->data()[j] += learning_rate * v[i]->data()[j];
