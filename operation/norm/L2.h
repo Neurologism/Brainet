57167bf5eec1c033206a5746aaba0ea5a9252d4a
Servant-of-Scietia <samsun2006@icloud.com>
2024-08-05 17:17:19 +0200

    L2 norm operation

---
+++ operation/norm/L2.h
@@ -0,0 +1,83 @@
+#ifndef L2_NORM_INCLUDE_GUARD
+#define L2_NORM_INCLUDE_GUARD
+
+#include "../operation/operation.h"
+
+/**
+ * @brief Used to add a L2 norm penalty to the graph. This is used to prevent overfitting.
+ */
+class L2_NORM : public OPERATION
+{
+    double _lambda;
+
+public:
+
+    /**
+     * @brief add a L2 norm penalty to the graph
+     * @param lambda the lambda value to be used
+     */
+    L2_NORM(double lambda);
+    ~L2_NORM() = default;
+    /**
+     * @brief compute the L2 norm of the input tensor
+    */
+    void f(std::vector<std::shared_ptr<VARIABLE>>& inputs) override;
+    /**
+     * @brief compute the gradient of the L2 norm penalty with respect to the input tensor
+     * @param inputs the parents of the variable
+     * @param focus this is the only variable everything else is constant
+     * @param gradient the sum of the gradients of the consumers
+    */
+    std::shared_ptr<TENSOR<double>> bprop(std::vector<std::shared_ptr<VARIABLE>>& inputs, std::shared_ptr<VARIABLE> & focus, std::shared_ptr<TENSOR<double>> & gradient) override;
+};
+
+L2_NORM::L2_NORM(double lambda)
+{
+    _lambda = lambda;
+}
+
+void L2_NORM::f(std::vector<std::shared_ptr<VARIABLE>>& inputs)
+{
+    if (inputs.size() != 1)
+    {
+        throw std::runtime_error("L2_NORM: number of inputs is not 1");
+    }
+
+    auto input = inputs[0]->get_data();
+    auto result = std::make_shared<TENSOR<double>>(TENSOR<double>({1}));
+
+    double sum = 0;
+    for (std::uint32_t i = 0; i < input->size(); i++)
+    {
+        sum += input->at({i}) * input->at({i});
+    }
+
+    result->set({0}, 0.5 * _lambda * sum);
+
+    this->get_variable()->get_data() = result;
+}
+
+
+std::shared_ptr<TENSOR<double>> L2_NORM::bprop(std::vector<std::shared_ptr<VARIABLE>>& inputs, std::shared_ptr<VARIABLE> & focus, std::shared_ptr<TENSOR<double>> & gradient)
+{
+    if (inputs.size() != 1)
+    {
+        throw std::runtime_error("L2_NORM: number of inputs is not 1");
+    }
+
+    auto input = inputs[0]->get_data();
+    auto result = std::make_shared<TENSOR<double>>(input->shape());
+
+    for (std::uint32_t i = 0; i < input->size(); i++)
+    {
+        result->data()[i] = _lambda * input->data()[i] * gradient->data()[0];
+    }
+
+    return result;
+}
+
+
+
+
+
+#endif // L2_NORM_INCLUDE_GUARD
