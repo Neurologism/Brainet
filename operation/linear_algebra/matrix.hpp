fdf381e57b90b0b9030e1a227d0a6ed71b1041e9
Servant-of-Scietia <samsun2006@icloud.com>
2024-08-12 16:24:39 +0200

    refactor tensor.hpp and extend matrix.hpp

--- operation/linear_algebra/matrix.hpp
+++ operation/linear_algebra/matrix.hpp
@@ -9,18 +9,107 @@
 template <typename T>
 class Matrix : public Tensor<T>
 {
-
+    typedef std::vector<T> DataVector;
+    typedef std::uint32_t ShapeType;
+    typedef std::vector<ShapeType> ShapeVector;
 
 public:
+    Matrix() = default;
 
+    /**
+     * @brief Construct a new Matrix object. The matrix is initialized with random values.
+     * @param dimensionality The dimensionality of the matrix.
+     */
+    Matrix(const ShapeVector & dimensionality) : Tensor<T>(dimensionality);
+
+    /**
+     * @brief Construct a new Matrix object. The matrix is initialized with a given value.
+     * @param dimensionality The dimensionality of the matrix.
+     * @param value The value to initialize the matrix with.
+     */
+    Matrix(const ShapeVector & dimensionality, const T & value) : Tensor<T>(dimensionality, value);
+
+    /**
+     * @brief Construct a new Matrix object.
+     * @param data The data to initialize the matrix with.
+     */
+    Matrix(const std::vector<std::vector<T>> & data) : Tensor<T>({data.size(), data[0].size()}, 0);
+
+    ~Matrix() = default;
+
+    /**
+     * @brief Access the element at the given position.
+     * @param i The row index.
+     * @param j The column index.
+     * @return The element at the given position.
+     */
+    T at(const std::uint32_t & i, const std::uint32_t & j);
+
+    /**
+     * @brief Set the element at the given position.
+     * @param i The row index.
+     * @param j The column index.
+     * @param value The value to set the element to.
+     */
+    void set(const std::uint32_t & i, const std::uint32_t & j, const T & value);
+
+    /**
+     * @brief Transpose the matrix.
+     * @return A new matrix that is the transposed version of the original matrix.
+     */
     std::shared_ptr<Matrix<T>> transpose();
 };
+
+template <typename T>
+Matrix<T>::Matrix(const ShapeVector & dimensionality) : Tensor<T>(dimensionality)
+{
+    if (dimensionality.size() != 2)
+        throw std::invalid_argument("Matrix::Matrix: Matrix must have exactly two dimensions.");
+}
+
+template <typename T>
+Matrix<T>::Matrix(const ShapeVector & dimensionality, const T & value) : Tensor<T>(dimensionality, value)
+{
+    if (dimensionality.size() != 2)
+        throw std::invalid_argument("Matrix::Matrix: Matrix must have exactly two dimensions.");
+}
+
+template <typename T>
+Matrix<T>::Matrix(const std::vector<std::vector<T>> & data) : Tensor<T>({data.size(), data[0].size()}, 0)
+{
+    for (std::uint32_t i = 0; i < data.size(); i++)
+    {
+        for (std::uint32_t j = 0; j < data[0].size(); j++)
+        {
+            mData[i * mShape[1] + j] = data[i][j];
+        }
+    }
+}
+
+template <typename T>
+T Matrix<T>::at(const std::uint32_t & i, const std::uint32_t & j)
+{
+    if (i >= mShape[0] || j >= mShape[1])
+        throw std::out_of_range("Matrix::at: Index out of range.");
+
+    return mData[i * mShape[1] + j];
+}
+
+template <typename T>
+void Matrix<T>::set(const std::uint32_t & i, const std::uint32_t & j, const T & value)
+{
+    if (i >= mShape[0] || j >= mShape[1])
+        throw std::out_of_range("Matrix::set: Index out of range.");
+
+    mData[i * mShape[1] + j] = value;
+}
 
 template <class T>
 std::shared_ptr<Matrix<T>> Matrix<T>::transpose()
 {
     if(mShape.size() != 2)
-        throw std::invalid_argument("Tensor::transpose: Tensor must be 2D");
+        throw std::invalid_argument("Matrix::transpose: Matrix must have exactly two dimensions.");
+
     std::shared_ptr<Tensor<T>> _tensor = std::make_shared<Tensor<T>>(Tensor<T>({mShape[1],mShape[0]})); // create a new tensor with the transposed shape
     DataVector _data(mData.size());
     // create a vector with the transposed data
--- operation/linear_algebra/tensor.hpp
+++ operation/linear_algebra/tensor.hpp
@@ -28,21 +28,20 @@ public:
      * @brief Construct an empty new Tensor object.
      */
     Tensor() = default;
+    
+    /**
+     * @brief Construct a new Tensor object. The tensor is initialized with random values.
+     * @param dimensionality The dimensionality of the tensor.
+     */
+    Tensor(const ShapeVector & dimensionality);
 
     /**
      * @brief Construct a new Tensor object. The tensor is initialized with a given value.
      * @param dimensionality The dimensionality of the tensor.
      * @param value The value to initialize the tensor with.
      */
-    Tensor(ShapeVector dimensionality, T value);
-
-
-    /**
-     * @brief Construct a new Tensor object. The tensor is initialized with random values.
-     * @param dimensionality The dimensionality of the tensor.
-     */
-    Tensor(ShapeVector dimensionality);
-
+    Tensor(const ShapeVector & dimensionality, const T & value);
+    
     ~Tensor() = default;
     
     /**
@@ -50,62 +49,85 @@ public:
      * @param index The indices of the element.
      * @return The element at the given position.
      */
-    T at(ShapeVector index);
+    T at(const ShapeVector & index);
 
     /**
      * @brief This function is used to access the data of the tensor with a single index.
      * @param index The index of the element.
      * @return The element at the given index.
      */
-    T at(ShapeType index);
+    T at(const ShapeType & index);
 
     /**
      * @brief This function is used to set the value of an element in the tensor. To do so it uses a vector of indices.
      * @param index The indices of the element.
      * @param value The value to be set.
      */
-    void set(ShapeVector index, T value);
+    void set(const ShapeVector & index, const T & value);
 
     /**
      * @brief This function returns the shape of the tensor.
      * @return The shape of the tensor.
      */
-    ShapeVector shape(){return mShape;};
+    ShapeVector shape();
 
     /**
      * @brief This function returns the shape of the tensor at a given index.
      * @param index The index of the shape.
      * @return The shape at the given index.
      */
-    ShapeType shape(ShapeType index){return mShape[index];};
+    ShapeType shape(const ShapeType & index);
 
     /**
      * @brief This function returns the dimensionality of the tensor.
-     * @return ShapeType The dimensionality of the tensor.
+     * @return The dimensionality of the tensor.
      */
-    ShapeType dimensionality(){return mShape.size();};
+    std::uint32_t dimensionality();
 
     /**
      * @brief This function returns the total capacity of the tensor.
      * @return The size of the tensor.
      */
-    ShapeType size(){return mData.size();};
+    std::uint32_t capacity();
 
     /**
      * @brief This function resizes the tensor.
      * @param dimensionality The new dimensionality of the tensor.
      */
-    void resize(ShapeVector dimensionality);
+    void resize(const ShapeVector & dimensionality);
 
     /**
      * @brief This function reshapes the tensor.
      * @param dimensionality The new dimensionality of the tensor.
      */
-    void reshape(ShapeVector dimensionality);
+    void reshape(const ShapeVector & dimensionality);
 };
 
 template <class T>
-T Tensor<T>::at(ShapeVector index)
+Tensor<T>::Tensor(const ShapeVector & dimensionality)
+{
+    mData = {};
+    mShape = dimensionality;
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::normal_distribution<T> dis(0, 1.0E-8);
+    for(std::uint32_t i = 0; i < mData.size(); i++)
+    {
+        mData.push_back(dis(gen));
+    }
+}
+
+template <class T>
+Tensor<T>::Tensor(const ShapeVector & dimensionality, const T & value)
+{
+    mData = DataVector(std::accumulate(dimensionality.begin(),dimensionality.end(),1, std::multiplies<ShapeType>()), value); // initialize the data vector
+    mShape = dimensionality; // set the shape of the tensor
+}
+
+
+template <class T>
+T Tensor<T>::at(const ShapeVector & index)
 {
     if(index.size() != mShape.size())
         throw std::invalid_argument("Tensor::at: Index size does not match the dimensionality of the tensor");
@@ -113,7 +135,7 @@ T Tensor<T>::at(ShapeVector index)
     ShapeType _block_size = std::accumulate(mShape.begin(), mShape.end(), 1, std::multiplies<ShapeType>()); // product of all dimensions
     ShapeType _index = 0;
     // calculate the index of the element
-    for (ShapeType i = 0; i < index.size(); i++)
+    for (std::uint32_t i = 0; i < index.size(); i++)
     {
         _block_size /= mShape[i];
         _index += index[i] * _block_size;
@@ -124,54 +146,23 @@ T Tensor<T>::at(ShapeVector index)
 }
 
 template <class T>
-T Tensor<T>::at(ShapeType index)
+T Tensor<T>::at(const ShapeType & index)
 {
     if(index >= mData.size())
         throw std::out_of_range("Index out of range");
     return mData[index]; // return the element
 }
 
-template <class T>
-Tensor<T>::Tensor(ShapeVector dimensionality)
-{
-    mData = {};
-    mShape = dimensionality;
-
-    std::random_device rd;
-    std::mt19937 gen(rd());
-    std::normal_distribution<double> dis(0, 1.0E-8);
-    for(ShapeType i = 0; i < mData.size(); i++)
-    {
-        mData.push_back(dis(gen));
-    }
-}
 
 template <class T>
-Tensor<T>::Tensor(ShapeVector dimensionality, T value)
-{
-    mData = DataVector(std::accumulate(dimensionality.begin(),dimensionality.end(),1, std::multiplies<ShapeType>()), value); // initialize the data vector
-    if(random) // if random is true, initialize the tensor with random values
-    {
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::normal_distribution<double> dis(0, 0.0001);
-        for(ShapeType i = 0; i < mData.size(); i++)
-        {
-            mData[i] = dis(gen);
-        }
-    }
-    mShape = dimensionality; // set the shape of the tensor
-}
-
-template <class T>
-void Tensor<T>::set(ShapeVector index, T value)
+void Tensor<T>::set(const ShapeVector & index, const T & value)
 {
     if(index.size() != mShape.size())
         throw std::invalid_argument("Tensor::set: Index size does not match the dimensionality of the tensor");
     ShapeType _block_size = std::accumulate(mShape.begin(), mShape.end(), 1, std::multiplies<ShapeType>()); // product of all dimensions
     ShapeType _index = 0;
     // calculate the index of the element
-    for (ShapeType i = 0; i < index.size(); i++)
+    for (std::uint32_t i = 0; i < index.size(); i++)
     {
         _block_size /= mShape[i];
         _index += index[i] * _block_size;
@@ -182,10 +173,43 @@ void Tensor<T>::set(ShapeVector index, T value)
 }
 
 template <class T>
-void Tensor<T>::reshape(ShapeVector dimensionality)
+Tensor<T>::ShapeVector Tensor<T>::shape()
+{
+    return mShape; // return the shape
+}
+
+template <class T>
+Tensor<T>::ShapeType Tensor<T>::shape(const ShapeType & index)
+{
+    if(index >= mShape.size())
+        throw std::out_of_range("Tensor::shape: Index out of range");
+    return mShape[index]; // return the shape at the given index
+}
+
+template <class T>
+std::uint32_t Tensor<T>::dimensionality()
+{
+    return mShape.size(); // return the dimensionality
+}
+
+template <class T>
+std::uint32_t Tensor<T>::capacity()
+{
+    return mData.size(); // return the capacity
+}
+
+template <class T>
+void Tensor<T>::resize(const ShapeVector & dimensionality)
+{
+    mData.resize(std::accumulate(dimensionality.begin(),dimensionality.end(),1, std::multiplies<ShapeType>())); // resize the data vector
+    mShape = dimensionality; // set the new shape
+}
+
+template <class T>
+void Tensor<T>::reshape(const ShapeVector & dimensionality)
 {
     if(std::accumulate(dimensionality.begin(),dimensionality.end(),1, std::multiplies<ShapeType>()) != mData.size())
-        throw std::invalid_argument("Tensor::reshape: New dimensionality does not match the size of the tensor");
+        throw std::invalid_argument("Tensor::reshape: New dimensionality does not match the capacity of the tensor");
     mShape = dimensionality; // set the new shape
 }
 
