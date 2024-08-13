b5ff54fb180336048ee6373ba4f5a18d4f5144f7
Servant-of-Scietia <samsun2006@icloud.com>
2024-08-13 17:50:37 +0200

    refactor Part 1

--- module/dense.hpp
+++ module/dense.hpp
@@ -13,23 +13,23 @@
 class Dense : public Module
 {
     // storing index of the variables in the graph
-    std::shared_ptr<Variable> _weight_matrix_variable; // learnable parameters of the layer (weights + bias)
-    std::shared_ptr<Variable> _matmul_variable; // multiplication of the input and the weights
-    std::shared_ptr<Variable> _activation_variable; // activation function applied
-    std::shared_ptr<Variable> _padding_variable; // used to pad the input with 1s for the bias
-    std::shared_ptr<Variable> _norm_variable; // used to compute a norm of the weights
+    std::shared_ptr<Variable> mpWeightMatrixVariable; // learnable parameters of the layer (weights + bias)
+    std::shared_ptr<Variable> mpMatmulVariable; // multiplication of the input and the weights
+    std::shared_ptr<Variable> mpActivationVariable; // activation function applied
+    std::shared_ptr<Variable> mpPaddingVariable; // used to pad the input with 1s for the bias
+    std::shared_ptr<Variable> mpNormVariable; // used to compute a norm of the weights
 
-    static std::shared_ptr<NormVariant> _default_norm; // default norm to use
-    std::shared_ptr<Operation> _norm = nullptr; // norm to use for regularization
+    static std::shared_ptr<NormVariant> mpsDefaultNorm; // default norm to use
+    std::shared_ptr<Operation> mpNorm = nullptr; // norm to use for regularization
 
 
 public:
     /**
      * @brief add a dense layer to the graph
-     * @param activation_function the operation representing the activation function.
+     * @param activationFunction the operation representing the activation function.
      * @param units the number of neurons in the layer.
      */
-    Dense(ActivationVariant activation_function, std::uint32_t units);
+    Dense(ActivationVariant activationFunction, std::uint32_t units);
     /**
      * @brief add a dense layer to the graph
      * @param activation_function the operation representing the activation function.
@@ -43,24 +43,24 @@ public:
      */
     void add_input(std::shared_ptr<Variable> input, std::uint32_t input_units) override
     {
-        _padding_variable->get_inputs().push_back(input);
+        mpPaddingVariable->get_inputs().push_back(input);
         
         // init default norm
-        if(_norm == nullptr && _default_norm != nullptr)
+        if(mpNorm == nullptr && mpsDefaultNorm != nullptr)
         {
-            _norm = std::visit([](auto&& arg) {
+            mpNorm = std::visit([](auto&& arg) {
                 // Assuming all types in the variant can be dynamically casted to Operation*
-                return std::shared_ptr<Operation>(std::make_shared<std::decay_t<decltype(arg)>>(arg));}, *_default_norm);
+                return std::shared_ptr<Operation>(std::make_shared<std::decay_t<decltype(arg)>>(arg));}, *mpsDefaultNorm);
         }
         
         // +1 for the weight
-        _weight_matrix_variable->get_data() = std::make_shared<Tensor<double>>(Tensor<double>({input_units+1,__units})); // initialize the weights randomly
+        mpWeightMatrixVariable->get_data() = std::make_shared<Tensor<double>>(Tensor<double>({input_units+1,__units})); // initialize the weights randomly
         
-        if (_norm != nullptr) // adding norm to activation function
+        if (mpNorm != nullptr) // adding norm to activation function
         {
-            _norm_variable = __graph->add_variable(std::make_shared<Variable>(Variable(_norm, {_weight_matrix_variable}, {})));
-            __graph->add_output(_norm_variable);    
-            _weight_matrix_variable->get_consumers().push_back(_norm_variable);
+            mpNormVariable = sGraph->add_variable(std::make_shared<Variable>(Variable(mpNorm, {mpWeightMatrixVariable}, {})));
+            sGraph->add_output(mpNormVariable);    
+            mpWeightMatrixVariable->get_consumers().push_back(mpNormVariable);
         }
     }
     /**
@@ -68,35 +68,35 @@ public:
      */
     void add_output(std::shared_ptr<Variable> output) override
     {
-        _activation_variable->get_consumers().push_back(output);
+        mpActivationVariable->get_consumers().push_back(output);
     }
     /**
      * @brief used to get the input variables of the module specified by the index.
      */
     std::shared_ptr<Variable> input(std::uint32_t index) override
     {
-        return _padding_variable;
+        return mpPaddingVariable;
     }
     /**
      * @brief used to get the output variables of the module specified by the index.
      */
     std::shared_ptr<Variable> output(std::uint32_t index) override
     {
-        return _activation_variable;
+        return mpActivationVariable;
     }
     /**
      * @brief used to set the default norm to use for regularization.
      */
-    static void set_default_norm(NormVariant const & norm)
+    static void setmpsDefaultNorm(NormVariant const & norm)
     {
-        _default_norm = std::make_shared<NormVariant>(norm);
+        mpsDefaultNorm = std::make_shared<NormVariant>(norm);
     }
 };
 
 Dense::Dense(ActivationVariant activation_function, std::uint32_t units)
 {
     // error checks
-    if(__graph == nullptr)
+    if(sGraph == nullptr)
     {
         throw std::runtime_error("graph is not set");
     }
@@ -104,12 +104,12 @@ Dense::Dense(ActivationVariant activation_function, std::uint32_t units)
 
     // create the variables
 
-    _padding_variable = __graph->add_variable(std::make_shared<Variable>(Variable(std::make_shared<Padding>(Padding(0,1,1)), {}, {}))); // pad for weights
+    mpPaddingVariable = sGraph->add_variable(std::make_shared<Variable>(Variable(std::make_shared<Padding>(Padding(0,1,1)), {}, {}))); // pad for weights
     
-    _weight_matrix_variable = __graph->add_variable(std::make_shared<Variable>(Variable(nullptr, {}, {}))); // nullptr because there is no operation
-    __learnable_parameters.push_back(_weight_matrix_variable);
+    mpWeightMatrixVariable = sGraph->add_variable(std::make_shared<Variable>(Variable(nullptr, {}, {}))); // nullptr because there is no operation
+    __learnable_parameters.push_back(mpWeightMatrixVariable);
 
-    _matmul_variable = __graph->add_variable(std::make_shared<Variable>(Variable(std::make_shared<Matmul>(Matmul()), {_padding_variable,_weight_matrix_variable}, {})));
+    mpMatmulVariable = sGraph->add_variable(std::make_shared<Variable>(Variable(std::make_shared<Matmul>(Matmul()), {mpPaddingVariable,mpWeightMatrixVariable}, {})));
 
     // turning the variant into a shared pointer to the operation class
     // Use std::visit to handle the variant
@@ -117,25 +117,25 @@ Dense::Dense(ActivationVariant activation_function, std::uint32_t units)
         // Assuming all types in the variant can be dynamically casted to Operation*
         return std::shared_ptr<Operation>(std::make_shared<std::decay_t<decltype(arg)>>(arg));}, ActivationVariant{activation_function});
 
-    _activation_variable = __graph->add_variable(std::make_shared<Variable>(Variable(operation_ptr, {_matmul_variable}, {})));
+    mpActivationVariable = sGraph->add_variable(std::make_shared<Variable>(Variable(operation_ptr, {mpMatmulVariable}, {})));
 
     // conections within the module
-    _padding_variable->get_consumers().push_back(_matmul_variable);
-    _weight_matrix_variable->get_consumers().push_back(_matmul_variable);
+    mpPaddingVariable->get_consumers().push_back(mpMatmulVariable);
+    mpWeightMatrixVariable->get_consumers().push_back(mpMatmulVariable);
     
-    _matmul_variable->get_consumers().push_back(_activation_variable);    
+    mpMatmulVariable->get_consumers().push_back(mpActivationVariable);    
 
     
 }
 
 Dense::Dense(ActivationVariant activation_function, std::uint32_t units, NormVariant norm)
 {
-    _norm = std::visit([](auto&& arg) {
+    mpNorm = std::visit([](auto&& arg) {
         // Assuming all types in the variant can be dynamically casted to Operation*
         return std::shared_ptr<Operation>(std::make_shared<std::decay_t<decltype(arg)>>(arg));}, norm);
     Dense(activation_function, units);
 }
 
-std::shared_ptr<NormVariant> Dense::_default_norm = nullptr;
+std::shared_ptr<NormVariant> Dense::mpsDefaultNorm = nullptr;
 
 #endif // DENSE_HPP
